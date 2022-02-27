7c048e36e3af759337b86055d2247c768f3ed37e
verowangari <verowangari34@gmail.com>
2022-02-27 18:46:18 +0300

    store the value of API key inside news-api_key

--- app/utils.py
+++ app/utils.py
@@ -1,2 +1,13 @@
 import requests
 from decouple import config
+
+NEWS_API_KEY = config('NEWS_API_KEY')
+COUNTRY = 'in'
+
+
+def get_latest_news():
+    '''
+    Function that gets the json response to our url request
+    '''
+    news_data = requests.get(f'https://newsapi.org/v2/top-headlines?country={COUNTRY}&apiKey={NEWS_API_KEY}').json()
+    return news_data['articles']
