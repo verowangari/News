31e680b3590a97b3817a8232b20b3c207bc82bd8
verowangari <verowangari34@gmail.com>
2022-02-27 21:48:40 +0300

    create testconfig class

--- config.py
+++ config.py
@@ -8,5 +8,18 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = False
     MAIL_DEBUG = False
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
 
 
