b410f3a2e440ba02f7f8172f27344a9a9ed12f61
verowangari <verowangari34@gmail.com>
2022-02-27 19:15:37 +0300

    create  a view function that gets the latest news data

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import app
 from flask import Flask
 from flask import render_template    # Add this line
-
+from .utils import get_latest_news 
 app = Flask (__name__)
 
 @app.route('/')
@@ -11,3 +11,7 @@ def say_hello():
 app.run(
     debug = True
 )
+@app.route('/news')
+def news_headlines():
+    news_articles = get_latest_news()
+    return render_template("news.html", news_articles=news_articles)
