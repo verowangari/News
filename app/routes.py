18d750c7760dc813aa78308e874bc31757ad04f3
verowangari <verowangari34@gmail.com>
2022-02-27 17:15:59 +0300

    initialize app serve to test

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
-from app import app
-
+from flask import app
+from flask import Flask
 from flask import render_template    # Add this line
 
+app = Flask (__name__)
 
 @app.route('/')
 def say_hello():
     user = {"name": "Wangari"}
     return render_template("index.html", user=user)    # Modify this line
+app.run(
+    debug = True
+)
