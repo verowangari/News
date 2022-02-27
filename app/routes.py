9ce44798b863f59d4e18013a4851c7b934adcdc5
verowangari <verowangari34@gmail.com>
2022-02-27 15:31:36 +0300

    import app and render template to renderthe html page

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app import app
+from flask import render_template    # Add this line
+
+
+@app.route('/')
+def say_hello():
+    user = {"name": "Wangari"}
+    return render_template("index.html", user=user)    # Modify this line
