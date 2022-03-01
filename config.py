1448b583f8cd788a8533b691553bee57a1296137
verowangari <verowangari34@gmail.com>
2022-03-01 08:32:31 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from distutils.log import debug
 
 
-NEWS_API_KEY='26e92efd5ae74abf9827342828a892d9'
+# NEWS_API_KEY='26e92efd5ae74abf9827342828a892d9'
 class Config(object):
     SECRET_KEY = 'guess-me'
     debug = False
